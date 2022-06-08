6bb165788c4f99f98c8fa860509cf3ad0084b47e
Danil <danilsit@gmail.com>
2022-06-08 23:34:26 +0300

    Add Seminar 9 files

---
+++ ZnakomstvoC#_HomeWork/Seminar_9/Exercise_01-PokazatNaturalnijeChislaOtMdoN,NiMZadani/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+void Recursion(int m, int n)
+{
+    if(m<n) Console.Write($"{m} ");
+    if(m==n) Console.Write($"{m} ");
+    if(m>n) return;
+    Recursion(m+1,n);
+}
+
+Console.Clear();
+int M = 2;
+int N = 15;
+Recursion(M,N);
---
+++ ZnakomstvoC#_HomeWork/Seminar_9/Exercise_02-NaitiSummuElementovOtMdoN,NiMZadani/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+int RecursionSum(int m, int n)
+{
+    if (m > n) return 0;
+    else
+    {
+        Console.Write($"{m} ");
+        return m + RecursionSum(m + 1, n);
+    }
+}
+
+Console.Clear();
+int M = 1;
+int N = 6;
+Console.Write($"  |  Sum of elements from {M} to {N} = {RecursionSum(M, N)} ");
